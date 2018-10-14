57427f25d5dd646c48085093cb3d032da7c3d509
vovo <vovo@vovoui-MacBook-Pro.local>
2018-10-14 13:29:55 +0900

    post new

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ app_name = 'blog'
 
 urlpatterns = [
     re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^post_detail/(?P<pk>\d+)', views.post_detail, name='post_detail'),
+    re_path(r'^post_new/$', views.post_new, name="post_new"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -11,3 +12,25 @@ def post_list(request):
                   {
                       "qs": qs
                   })
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(request, 'blog/post_detail.html', {
+        "post": post
+    })
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post)
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_form.html', {
+        'form': form
+    })
