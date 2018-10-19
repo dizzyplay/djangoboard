d9e5732ba0e8b0b683aa4c104ef54df1e4eb0983
vovo <vovoboss@gmail.com>
2018-10-20 02:00:50 +0900

    cors setting

--- djangotest/settings/settings.py
+++ djangotest/settings/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+CORS_ORIGIN_WHITELIST = (
+    '211.114.88.77',
+    'localhost:8000',
+    '127.0.0.1:8000'
+)
