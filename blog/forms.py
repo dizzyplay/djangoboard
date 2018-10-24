e4e8f3b7eba7bc30cdd5b535943f6d09a949b1e0
vovo <vovoboss@gmail.com>
2018-10-24 17:55:26 +0900

    부트스트랩 적용

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,18 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control form-control-sm',
+                'id': 'title'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control form-control-sm',
+                'rows': '10',
+                'id': 'content',
+            }),
+        }
+        labels = {
+            "title": "제목",
+            "content": "내용",
+        }
