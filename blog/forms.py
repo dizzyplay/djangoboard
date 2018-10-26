6cec3890399c5abb9f610d4745a39ba6b2805d2d
vovo <vovoboss@gmail.com>
2018-10-26 20:00:44 +0900

    Mod category function

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,7 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['title', 'content']
         widgets = {
             'title': forms.TextInput(attrs={
                 'class': 'form-control form-control-sm',
--- blog/views.py
+++ blog/views.py
@@ -2,23 +2,24 @@ from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
 from .models import Post, Category
 from .forms import PostForm
-
-# Create your views here.
+from .context_processors import category_context
 
 
 def post_list(request):
-    category = request.GET.get('category', None)
+    category = category_context(request)['category_title']
     if category:
         try:
             category_q = Category.objects.get(title=category)
             qs = Post.objects.filter(category=category_q)
         except ObjectDoesNotExist:
-            qs = Post.objects.all()
+            print('object does not exist!!!')
     else:
-        qs = Post.objects.all()
+        category_q = Category.objects.get(title='자유')
+        qs = Post.objects.filter(category=category_q)
     return render(request, 'blog/post_list.html',
                   {
-                      "qs": qs
+                      "qs": qs,
+                      "category_title": category_q,
                   })
 
 
@@ -26,19 +27,30 @@ def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
 
     return render(request, 'blog/post_detail.html', {
-        "post": post
+        "post": post,
+        "category_title": post.category,
     })
 
 
 def post_new(request):
+    category = category_context(request)['category_title']
+    print(category)
+    if category:
+        try:
+            category_q = Category.objects.get(title=category)
+        except ObjectDoesNotExist:
+            return redirect('blog:post_list')
+
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
+            post = form.save(commit=False)
+            post.category = category_q
             post = form.save()
             return redirect(post)
     else:
         form = PostForm()
 
     return render(request, 'blog/post_form.html', {
-        'form': form
+        'form': form,
     })
