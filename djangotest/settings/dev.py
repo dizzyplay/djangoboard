0f31eeed3bc572e9de609b11237759cfb9677765
vovo <vovoboss@gmail.com>
2018-10-29 00:35:28 +0900

    Email setting

--- djangotest/settings/dev.py
+++ djangotest/settings/dev.py
@@ -1,6 +1,12 @@
 from .settings import *
+import json
 
 DEBUG = True
+
+#비밀키관련 json파일 불러오기
+with open(BASE_DIR+"\secret.json","r") as f:
+    secrets = json.loads(f.read())
+
 
 DATABASES = {
     'default': {
@@ -8,3 +14,11 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = secrets["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = secrets["EMAIL_HOST_PASSWORD"]
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
