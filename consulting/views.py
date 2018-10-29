3c6c8a40fe9206d88a1380a62fd2dfedfb15d962
vovo <vovoboss@gmail.com>
2018-10-29 18:31:27 +0900

    Add email seding function

--- consulting/views.py
+++ consulting/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage, send_mail
+from django.template import loader
 from .forms import ProductRequestForm
 
 
@@ -10,11 +12,31 @@ def test_view(request):
     if request.method == 'POST':
         form = ProductRequestForm(request.POST)
         if form.is_valid():
-            form.save()
+            post = form.save()
+            html_message = loader.render_to_string(
+                'consulting/email_html.html',
+                {
+                    'user_name':post.customer_name,
+                    'subject': post.project_info,
+                    'phone': post.customer_phone,
+                    'request_category': post.get_request_category_display,
+                    'email':post.customer_email,
+                    'price':post.customer_price,
+                    'request_date': post.customer_request_date,
+                    'delivery_date': post.customer_delivery_date,
+                    'memo': post.customer_memo
+                }
+            )
+            content = "\n".join([post.customer_name, post.customer_phone, post.project_info])
+            # email = EmailMessage(post.customer_name+'님의 요청입니다', str(content) , to=['dizzyplay@naver.com'])
+            send_mail(post.customer_name+'님 안녕하세요','asdf','vovoboss@gmail.com',['dizzyplay@naver.com','studiofroots@gmail.com'],html_message=html_message)
+            print(post)
+            # email.send()
             return redirect('consulting:test_view')
     else:
         form = ProductRequestForm()
     return render(request, './consulting/test.html',{
         'form': form
     })
+
 
