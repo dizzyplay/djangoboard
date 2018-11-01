3817b0438e72552007b3348fb1fddd91e851f0ee
vovo <vovo@vovoui-MacBook-Pro.local>
2018-11-01 17:01:49 +0900

    비밀키 가져오기 윈도우/맥 분리

--- djangotest/settings/dev.py
+++ djangotest/settings/dev.py
@@ -15,15 +15,24 @@ DATABASES = {
 
 
 
-#비밀키관련 json파일 불러오기
-with open(BASE_DIR+"\secret.json","r") as f:
-    secrets = json.loads(f.read())
+#메일 설정 비밀키 윈도우에서  json파일 가져오기
+try:
+    with open(BASE_DIR+"\secret.json","r") as f:
+        secrets = json.loads(f.read())
+    email_host_user=secrets["EMAIL_HOST_USER"]
+    email_host_password=secrets["EMAIL_HOST_PASSWORD"]
+except:
+    pass
+
+#리눅스/맥에서 환경변수로 메일설정 비밀키 가져오기
+email_host_user=os.environ.get("EMAIL_HOST_USER", "")
+email_host_password=os.environ.get("EMAIL_USER_PASSWORD", "")
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = secrets["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = secrets["EMAIL_HOST_PASSWORD"]
+EMAIL_HOST_USER = email_host_user
+EMAIL_HOST_PASSWORD = email_host_password
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
