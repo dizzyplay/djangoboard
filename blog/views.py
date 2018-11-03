8cbf1e9ffa1e821b8752e940151f52d0683a841b
vovo <vovoboss@gmail.com>
2018-11-04 00:58:51 +0900

    Add Paginator

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.paginator import Paginator
 from .models import Post, Category
 from .forms import PostForm
 from .context_processors import category_context
@@ -7,10 +8,14 @@ from .context_processors import category_context
 
 def post_list(request):
     category = category_context(request)['category_title']
+    page = request.GET.get('page', 1)
+
     if category:
         try:
             category_q = Category.objects.get(title=category)
             qs = Post.objects.filter(category=category_q)
+            p = Paginator(qs, 1)
+            qs = p.get_page(page)
         except ObjectDoesNotExist:
             print('object does not exist!!!')
     else:
@@ -19,17 +24,27 @@ def post_list(request):
     return render(request, 'blog/post_list.html',
                   {
                       "qs": qs,
+                      "pages": p,
                       "category_title": category_q,
                   })
 
 
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
-    qs = Post.objects.filter(id=pk)
+    category = category_context(request)['category_title']
+    if category:
+        num = int(pk)
+        category= Category.objects.get(title=category)
+        qs_obj = Post.objects.filter(category=category)
+        p = Paginator(qs_obj, 5)
+        qs = p.page(1)
+
+        print(qs.object_list)
 
     return render(request, 'blog/post_detail.html', {
         "post": post,
         "category_title": post.category,
+        "qs":qs.object_list,
     })
 
 
