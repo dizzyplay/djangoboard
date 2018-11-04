e8ed25e6364750a96b77b8ef5e6c593fe089afdb
vovo <vovoboss@gmail.com>
2018-11-05 01:33:52 +0900

    Mod Paginator function

--- blog/paginator.py
+++ blog/paginator.py
@@ -2,17 +2,18 @@ from django.core.paginator import Paginator
 
 
 class CustomPaginator(Paginator):
-    def custom_page_range(self, a, b):
-        return range(int(a), int(b))
+    def custom_page_range(self, start, end):
+        return range(int(start), int(end))
 
 
-def page_range_check(page, max_page):
-    if (page - 5) < 1:
+def page_range_check(page, max_page, page_interval=5):
+    # page_interval 은 선택된 페이지 번호 포함한 개수
+    if (page - page_interval) < 1:
         page_start = 1
     else:
-        page_start = page - 5
+        page_start = page - (page_interval-1)
 
-    for i in range(5):
+    for i in range(page_interval+1):
         if (page + i) > max_page:
             page_end = max_page + 1
             break
@@ -20,3 +21,6 @@ def page_range_check(page, max_page):
             page_end = page + i
 
     return page_start, page_end
+
+
+
