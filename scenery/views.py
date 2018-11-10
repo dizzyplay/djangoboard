80abc0e7fa85bf07678fbfb411647404451efdf9
vovo <vovoboss@gmail.com>
2018-11-11 06:26:32 +0900

    for 15 minute

--- scenery/views.py
+++ scenery/views.py
@@ -6,19 +6,19 @@ from datetime import datetime, timedelta
 
 
 def main_view(request):
-    m_int = 5
-    m_delta = timedelta(minutes=5)
+    m_int = 15
+    m_delta = timedelta(minutes=15)
     now = datetime.now()
     testlist = []
     if now.minute % m_int == 0:
-        for i in range(10):
+        for i in range(5):
             testlist.append(now.strftime("%Y-%m-%d_%H%M"))
             now -= m_delta
     else:
         get_lid_value = now.minute % m_int
         get_lid_value_delta =timedelta(minutes=get_lid_value)
         now -= get_lid_value_delta
-        for i in range(10):
+        for i in range(5):
             testlist.append(now.strftime("%Y-%m-%d_%H%M"))
             now -= m_delta
 
