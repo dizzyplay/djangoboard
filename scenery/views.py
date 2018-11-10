6f0d2d82b675c3754c952cb4b37ce493127b901b
vovo <vovoboss@gmail.com>
2018-11-11 07:04:01 +0900

    Mod error

--- scenery/views.py
+++ scenery/views.py
@@ -7,9 +7,21 @@ from datetime import datetime, timedelta
 
 def main_view(request):
     period = int(request.GET.get('period', '60'))
+    request_time = request.GET.get('time',None)
+    if request_time is not None:
+        request_time = str(request_time)
+        year = int(request_time[:4])
+        month = int(request_time[4:6])
+        days = int(request_time[6:8])
+        hour = int(request_time[8:10])
+        minute = int(request_time[10:12])
+        print("{}년 {}월 {}일 {}시 {}분".format(year, month, days, hour, minute))
+        now = datetime(year, month, days, hour, minute)
+    else:
+        now = datetime.now()
+
     m_int = period
     m_delta = timedelta(minutes=period)
-    now = datetime.now()
     testlist = []
     if now.minute % m_int == 0:
         for i in range(10):
