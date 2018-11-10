d00190f6854f180e3a531c495a7ca028090098c8
vovo <vovoboss@gmail.com>
2018-11-11 06:37:09 +0900

    Add period

--- scenery/views.py
+++ scenery/views.py
@@ -6,19 +6,20 @@ from datetime import datetime, timedelta
 
 
 def main_view(request):
-    m_int = 15
-    m_delta = timedelta(minutes=15)
+    period = int(request.GET.get('period', '60'))
+    m_int = period
+    m_delta = timedelta(minutes=period)
     now = datetime.now()
     testlist = []
     if now.minute % m_int == 0:
-        for i in range(5):
+        for i in range(10):
             testlist.append(now.strftime("%Y-%m-%d_%H%M"))
             now -= m_delta
     else:
         get_lid_value = now.minute % m_int
-        get_lid_value_delta =timedelta(minutes=get_lid_value)
+        get_lid_value_delta = timedelta(minutes=get_lid_value)
         now -= get_lid_value_delta
-        for i in range(5):
+        for i in range(10):
             testlist.append(now.strftime("%Y-%m-%d_%H%M"))
             now -= m_delta
 
