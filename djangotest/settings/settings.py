484d1684e07c9acc21b81fe0492e249083670c7d
dizzyplay <vovoboss@gmail.com>
2018-11-19 16:40:20 +0900

    Add image app

--- djangotest/settings/settings.py
+++ djangotest/settings/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'users',
     'consulting',
     'scenery',
+    'image',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
